1a104dc08a02753568bf602a0ef4c9bd73262907
KaranDhawan38 <karandhawan2014@gmail.com>
2022-12-19 17:08:35 +0530

    Assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'assignments';
   username = '';
+  isParagraphVisible = false;
+  btnClickTimeStamps:any = [];
 
   resetUsername(){
     this.username = '';
+  }
+
+  toggleParagraph(){
+    this.btnClickTimeStamps.push({ 
+      "timestamp" : new Date().toString(),
+      "colorItem" : this.btnClickTimeStamps.length >= 4
+  });
+    this.isParagraphVisible = !this.isParagraphVisible;
   }
 }
